af796152c4ec7e2557673fa54c2c225c69317d0c
Cvilev Andrey <CvilevAS@mail.ru>
2023-04-05 19:40:13 +0500

    do something

--- cli.py
+++ cli.py
@@ -4,9 +4,8 @@ import argparse
 def parse():
     parser = argparse.ArgumentParser(
         prog='gendiff',
-        description='Compares two '
-                    'configuration files and '
-                    'shows a difference.')
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
