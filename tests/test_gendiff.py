3b275a3e2e388d1802d707d94799c83cf8722af9
Cvilev Andrey <CvilevAS@mail.ru>
2023-08-12 19:42:30 +0500

    add files

---
+++ tests/test_gendiff.py
@@ -0,0 +1,85 @@
+from gendiff.generate_diff import generate_diff
+from gendiff.tree import get_dictionary_from_file
+from gendiff.tree import generate_diff_tree
+import gendiff.formatters.stylish as stylish
+import pytest
+
+
+@pytest.fixture
+def dictionary1():
+    return {'host': 'hexlet.io', 'timeout': 50,
+            'proxy': '123.234.53.22', 'follow': False}
+
+
+@pytest.fixture
+def dict_string():
+    return ("{\n        host: hexlet.io\n        timeout: 50\
+            n        proxy: 123.234.53.22\n        follow: false\n    }")
+
+
+def test_dict():
+    file1_dict = get_dictionary_from_file('tests/fixtures/file1.json')
+    assert file1_dict == {'host': 'hexlet.io', 'timeout': 50,
+                          'proxy': '123.234.53.22', 'follow': False}
+
+
+def test_yaml(dictionary1):
+    file1_dict = get_dictionary_from_file('tests/fixtures/file1.yaml')
+    assert file1_dict == dictionary1
+
+
+def test_yml(dictionary1):
+    file1_dict = get_dictionary_from_file('tests/fixtures/file1.yml')
+    assert file1_dict == dictionary1
+
+
+def test_diff():
+    diff = ('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}')  # noqa: E501
+    assert generate_diff('tests/fixtures/file1.json',
+                         'tests/fixtures/file2.json').lower() == diff.lower()
+
+
+def test_tree1():
+    f = open('tests/fixtures/result.txt', 'r')
+    diff_strig = generate_diff('tests/fixtures/tree1.json',
+                               'tests/fixtures/tree2.json')
+    d = zip(diff_strig.split('\n'), f)
+    for k, v in d:
+        assert k.lower() == v.lower() or (k + '\n').lower() == v.lower()
+
+
+def test_tree_plain():
+    f = open('tests/fixtures/result_plain.txt', 'r')
+    diff_strig = generate_diff('tests/fixtures/tree1.json',
+                               'tests/fixtures/tree2.json', 'plain')
+    d = zip(diff_strig.split('\n'), f)
+    for k, v in d:
+        assert k.strip() == v.strip()
+
+
+def test_of_dict():
+    test_list = [(' ', 'common', [('+', 'follow', False),
+                                  (' ', 'setting1', 'Value 1'),
+                 ('-', 'setting2', 200), ('-+', 'setting3', True, 'null'),
+                 ('+', 'setting4', 'blah blah'), ('+', 'setting5',
+                                                  [(' ', 'key5', 'value5')]),
+                 (' ', 'setting6', [(' ', 'doge', [('-+', 'wow',
+                                                    '', 'so much')]),
+                                    (' ', 'key', 'value'),
+                                    ('+', 'ops', 'vops')])]),
+                 (' ', 'group1', [('-+', 'baz', 'bas', 'bars'),
+                                  (' ', 'foo', 'bar'),
+                                  ('-+', 'nest', [(' ', 'key', 'value')],
+                                   'str')])]
+    res = generate_diff_tree('tests/fixtures/short_tree1.json',
+                             'tests/fixtures/short_tree2.json')
+    print(test_list)
+    print(res)
+    assert test_list == res
+
+
+def test_of_str():
+    test_tree = [(' ', 'key1', 'value1'),
+                 ('+', 'key2', [(' ', 'key3', 'value2')])]
+    s = '{\n    key1: value1\n  + key2: {\n        key3: value2\n    }\n}'
+    assert stylish.stringify(test_tree) == s
