f170b3487aa1c9b9a0e5dd6df9549c3657b630a1
Cvilev Andrey <CvilevAS@mail.ru>
2023-09-10 14:42:13 +0500

    add files

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,5 +1,59 @@
 import json
+from gendiff.tree import DELETED, ADDED, NESTED, UNCHANGED, CHANGED
+
+# def make_format(node):
+#     return json.dumps(node, indent=2)
 
 
-def make_format(node):
-    return json.dumps(node, indent=2)
+def make_format(data):
+    return json.dumps(get_clean_diff(data), indent=2)
+
+
+def sort_diff(diff_tree):
+    sorted_dict = sorted(diff_tree, key=lambda d: d['key'])
+    for item in sorted_dict:
+        if item['type'] == NESTED:
+            item['value'] = sort_diff(item['value'])
+    return sorted_dict
+
+
+def get_clean_diff(diff):
+    clean_diff = []
+    for dct in diff:
+        clean_node = {
+            'type': dct['type'],
+            'key': dct['key']
+        }
+        if dct['type'] == ADDED\
+                or dct['type'] == DELETED\
+                or dct['type'] == UNCHANGED:
+            clean_node['value'] = dct['value']
+        if dct['type'] == CHANGED:
+            clean_node['value'] = dct['value'][0]
+            clean_node['value'] = dct['value'][1]
+        if dct['type'] == NESTED:
+            clean_node['value'] = get_clean_diff(dct['value'])
+
+        clean_diff.append(clean_node)
+
+    return sort_diff(clean_diff)
+
+
+# def make_format(node):
+#     output = []
+#     for item in node:
+#         if item['type'] == DELETED:
+#             output.append({'key': item['key'], 'value': item['value']})
+#         elif item['type'] == CHANGED and\
+#                 item['value'][0] != item['value'][1]:
+#             output.append({'type': CHANGED, 'key': item['key'], 'value': item['value'][0]})
+#             output.append({'key': item['key'], 'value': item['value'][1]})
+#         elif item['type'] == ADDED:
+#             output.append({'key': item['key'], 'value': item['value']})
+#         elif item['type'] == NESTED and item['value']:
+#             output.append(make_format({'key': item['key'], 'value': item['value']}))  
+#     return output
+
+
+# def make_format(node):
+#     return json.dumps(make_format2(node), indent=2)
